57da1130bb724f44c0d95264b94f782e5ff2eedc
AzizAbubshait <abubsh@gmail.com>
2018-01-24 19:19:26 -0500

    Here is the data and the script.

---
+++ MainScript.R
@@ -0,0 +1,8 @@
+# to do list:
+# 1) calculate percentage for each block separately
+# 2) get agency score
+# 3) get experience scores
+
+# agency questions:
+# experience questions:
+df <- read.csv("pilot data.csv")
