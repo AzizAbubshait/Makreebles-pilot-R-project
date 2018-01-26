9a67cfadee008446801d24c482e352bada3cd83d
Patrick Weis <ppweis@posteo.de>
2018-01-26 15:22:13 -0500

    preliminary rating descriptives

--- MainScript.R
+++ MainScript.R
@@ -5,6 +5,7 @@
 
 #### Library ####
 library(plyr)
+library(car)
 
 #### Data Readin ####
 # agency questions:
@@ -44,8 +45,9 @@ id=NaN
 agent=NaN
 preexposed=NaN
 quest=NaN
+scale=NaN
 answ=NaN
-df3=data.frame(id,agent,preexposed,quest,answ)
+df3=data.frame(id,agent,preexposed,quest,scale,answ)
 for (id in unique(df$ResponseId)){
   
   # Ratings
@@ -58,18 +60,26 @@ for (id in unique(df$ResponseId)){
       preexposed=0
     }
     quest=substr(name,6,nchar(name))
+    if (is.element(quest,c("fear","hunger","joy","pain","pleasure","desire"))) {
+      scale="experience"
+    }
+    if (is.element(quest,c("communication","morality","consciousness","emoRecog","planning","selfCont","conscious","personality","rage"))) {
+      scale="agency"
+    }
     answ=df[which(df$ResponseId==id),which(names(df)==name)]
-    df3=rbind(df3,c(id,agent,preexposed,quest,as.character(answ)))
+    df3=rbind(df3,c(id,agent,preexposed,quest,scale,as.character(answ)))
   }
 }
 df3=df3[-1,]
-df3$answ=recode(df3$answ,"'Extremely unikely'=1;'Moderately unlikely'=2;'Slightly unlikely'=3;'Neither likely nor unlikely'=4;)
+df3$answ=as.numeric(recode(df3$answ,"'Extremely unlikely'=1;'Moderately unlikely'=2;'Slightly unlikely'=3;'Neither likely nor unlikely'=4;'Slightly likely'=5;'Moderately likely'=6;'Extremely likely'=7"))
 
 #### data exploration ####
 df2.id.block=ddply(df2,.(id,block),summarise,
                    cor.pct=mean(cor))
 df2.block.ga=ddply(df2.id.block,.(block),summarise,
                    cor.pct=mean(cor.pct))
+df3.preexposed.scale=ddply(df3,.(preexposed,scale),summarise,
+                           rat=mean(answ,na.rm=T))
 
 
 ######## Graphs
